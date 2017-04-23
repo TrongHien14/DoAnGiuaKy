640f739ed0dd6f9d4d2fe9485512901b8dacbda8
TrongHien14 <hiendt49@gmail.com>
2017-04-23 16:32:24 +0700

    Da tao trang Products

---
+++ DoAnGiuaKy/DoAnGiuaKy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnGiuaKy.Startup))]
+namespace DoAnGiuaKy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
